5396c85d94f6202f6cfbddb90f9697a548529ae6
andrei-hs <123700875+andrei-hs@users.noreply.github.com>
2026-03-20 20:54:03 -0300

    Aula 2 Variáveis e Ambientes

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,29 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENAÇÃO DE STRINGS
+
+# Comentários em python de 1 linha
+'''
+Comentários de
+múltiplas
+linhas
+'''
+
+# Variáveis
+nome = "Alexandre" # String
+idade = 26 # Int
+peso = 70.2 # Float
+
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!!")
+
+nome_input = input("Digite o seu nome: ")
+idade_input = int(input("Digite a sua idade: "))
+nova_idade = idade_input + 1
+
+print(nome_input, idade_input)
+print(nova_idade)
+
+# print(f"Seu nome é {nome_input} e você tem {idade_input} anos")
---
+++ aula02-variaveis-operadores/desafios/desafio02.py
@@ -0,0 +1,5 @@
+dia = int(input("Digite o dia: "))
+mes = int(input("Digite o mês: "))
+ano = int(input("Digite o ano: "))
+print(f"A data que me informou é {dia}/{mes}/{ano}.")
+
